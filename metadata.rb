09c5366961553a17ab5a690235d35bafd39bc269
Clicia Scarlet <yuyuvn@mac.com>
2015-12-17 15:04:29 +0700

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name             'allegrograph'
 maintainer       'Clicia Scarlet'
-license          'All rights reserved'
+maintainer_email 'yuyuvn@me.com'
+license          'Apache Public License 2.0'
 description      'Installs/Configures AllegroGraph'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version IO.read(File.join(File.dirname(__FILE__), 'VERSION')).chomp rescue '0.1.2'
